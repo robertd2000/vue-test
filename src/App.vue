ae32f29db0c292de952c92b7af6abf2e21b52361
Rob <rob@example.com>
2022-11-23 21:46:43 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import Navigation from './components/Navigation.vue'
+import photo_1 from './assets/images/14896420-2 1 (1).png'
+</script>
+
+<template>
+  <Navigation />
+
+  <main>
+    <section class="main">
+      <div class="container">
+        <div class="slider">
+          <div class="main-image">
+            <img :src="photo_1" alt="" />
+          </div>
+        </div>
+
+        <div class="order">
+          <h3 class="title">Пижама для девочек</h3>
+
+          <span class="article"> Арт. 02765/46 </span>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style>
+.main {
+  margin-top: 24px;
+}
+
+.container {
+  max-width: 1390px;
+  margin: 0 auto;
+  display: flex;
+}
+
+.order {
+  margin-left: 30px;
+}
+
+.order.title {
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 24px;
+}
+
+.article {
+  font-size: 12px;
+  line-height: 16px;
+  color: #828282;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,42 @@
+<script setup>
+import logo from '../assets/svg/LOGO.svg'
+</script>
+
+<template>
+  <header>
+    <nav class="container">
+      <div class="logo">
+        <img :src="logo" alt="" />
+      </div>
+
+      <div class="icons">
+        <i class="fa-light fa-user"></i>
+        <i class="fa-light fa-heart"></i>
+        <i class="fa-light fa-bag-shopping"></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  background: #ffffff;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 8px 0;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+.icons {
+  color: black;
+  display: flex;
+}
+
+.icons i {
+  margin-left: 30px;
+}
+</style>
